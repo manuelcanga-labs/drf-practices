33ee86a8c8af73cbbbf06bd441f37f5f4a591110
Manuel Canga <php@manuelcanga.dev>
2026-05-02 19:16:07 +0200

    feat: Add endpoints

--- practice02/liga/views.py
+++ practice02/liga/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Equipo
+from .serializers import EquipoSerializer
 
 # Create your views here.
+class EquipoListCreate(generics.ListCreateAPIView):
+    queryset = Equipo.objects.all()
+    serializer_class = EquipoSerializer
+
+
+class EquipoRetrieve(generics.RetrieveAPIView):
+    queryset = Equipo.objects.all()
+    serializer_class = EquipoSerializer
+
