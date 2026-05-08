9114dcaf912d948fa9ac19f60d7e15dd05d83106
Manuel Canga <php@manuelcanga.dev>
2026-05-08 08:51:22 +0200

    feat(settings): All views needs Auhentication now

--- practice03/taskvault/settings.py
+++ practice03/taskvault/settings.py
@@ -60,6 +60,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = "taskvault.urls"
 
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
     ]
