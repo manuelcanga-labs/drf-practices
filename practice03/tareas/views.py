8974efc45cc861ee7945f098b0f74a60e8e6e086
Manuel Canga <php@manuelcanga.dev>
2026-05-08 09:38:21 +0200

    feat(User,views): Allow to anyone to create an user

--- practice03/tareas/views.py
+++ practice03/tareas/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from .serializers import UserSerializer
 
@@ -7,3 +8,4 @@ from .serializers import UserSerializer
 class CreateUser(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
