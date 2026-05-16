3e1f0ffd2246c2363a4791e6686e9f06aec438b3
Manuel Canga <php@manuelcanga.dev>
2026-05-16 10:09:02 +0200

    feat<models>: Add 'RecetaSecreta' model

--- practice04/menu/models.py
+++ practice04/menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class RecetaSecreta(models.Model):
+    nombre = models.CharField(max_length=100)
+    ingredientes = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+
