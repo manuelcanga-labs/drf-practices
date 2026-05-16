76ee42e3df0c6e99798dbd6dc852a2767f265e8c
Manuel Canga <php@manuelcanga.dev>
2026-05-16 13:14:35 +0200

    feat<auth>: Allow to use autoauth

--- practice04/cafesecreto/urls.py
+++ practice04/cafesecreto/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", views.obtain_auth_token),
     path("api/v1/", include("menu.urls")),
 ]
