d3df97d8b7159339a285fe1f7bdfa57aca5881b8
Manuel Canga <php@manuelcanga.dev>
2026-05-16 10:25:51 +0200

    feat<endpoint>: Create a new endpoint  in order to show and create new recetas

--- practice04/menu/views.py
+++ practice04/menu/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .models import RecetaSecreta
+from .serializers import RecetaSecretaSerializer
 
 # Create your views here.
+class Recetas(ListCreateAPIView):
+    queryset = RecetaSecreta.objects.all()
+    serializer_class = RecetaSecretaSerializer
+    permission_classes = [IsAuthenticated,]
+
